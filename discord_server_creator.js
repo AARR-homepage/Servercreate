9c8d87a77785e683b4bd18f90a5324d040635f81
AARR-homepage <qgebehhbwgrwfh@hotmail.com>
2025-05-06 22:45:21 +0900

    Update discord_server_creator.js

--- discord_server_creator.js
+++ discord_server_creator.js
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 const response = await fetch(`https://discord.com/api/v9/guilds`, {
                     method: 'POST',
                     headers: {
-                        'Authorization': `Bot ${token}`,
+                        'Authorization': `${token}`,
                         'Content-Type': 'application/json',
                     },
                     body: JSON.stringify(payload),
